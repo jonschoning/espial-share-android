d66092df27e3941ce03c2e816a4b96033c984a2b
Jon Schoning <jonschoning@gmail.com>
2021-09-19 21:44:07 -0500

    always allow script to close activity window

--- app/src/main/java/com/espial/espialshare/AddActivity.kt
+++ app/src/main/java/com/espial/espialshare/AddActivity.kt
@@ -2,11 +2,10 @@ package com.espial.espialshare
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
-import android.webkit.WebChromeClient
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
 import androidx.preference.PreferenceManager
 
 class AddActivity : AppCompatActivity() {
@@ -20,11 +19,25 @@ class AddActivity : AppCompatActivity() {
             settings.loadsImagesAutomatically = true
             settings.javaScriptEnabled = true
             scrollBarStyle = View.SCROLLBARS_INSIDE_OVERLAY
-            webViewClient = WebViewClient()
+            webViewClient = object : WebViewClient() {
+                override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError? ) {
+                    super.onReceivedError(view, request, error)
+                    Log.e("AddActivity::WebViewClient::onReceivedError", "errorCode:${error?.errorCode} description:${error?.description}")
+                }
+            }
             webChromeClient = object : WebChromeClient() {
                 override fun onCloseWindow(w: WebView?) {
                     super.onCloseWindow(w);
                     finish()
+                }
+                override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
+                    var ret = super.onConsoleMessage(consoleMessage)
+                    if(consoleMessage?.messageLevel() == ConsoleMessage.MessageLevel.WARNING &&
+                        consoleMessage?.message().startsWith("Scripts may close only the windows that were opened by them.", true)) {
+                        Log.d("AddActivity::WebChromeClient::onConsoleMessage", "Detected window script close attempt, finishing activity")
+                        finish()
+                    }
+                    return ret;
                 }
             }
         }
@@ -48,8 +61,8 @@ class AddActivity : AppCompatActivity() {
             Intent.ACTION_SEND -> {
                 if ("text/plain" == intent.type) {
                     val intentUrl = intent.getStringExtra(Intent.EXTRA_TEXT)
-                    val intentTitle = intent.getStringExtra(Intent.EXTRA_SUBJECT)
-                    val loadUrl = "$espialServerUrl/add?_hasData&url=$intentUrl&title=$intentTitle"
+                    val intentSubject = intent.getStringExtra(Intent.EXTRA_SUBJECT) ?: ""
+                    val loadUrl = "$espialServerUrl/add?_hasData&url=$intentUrl&title=${intentSubject}"
                     webView.loadUrl(loadUrl)
                 }
             }
