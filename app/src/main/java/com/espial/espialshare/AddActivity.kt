0164c0f6b064c5902f50a491ce9f80a8e6907616
Jon Schoning <jonschoning@gmail.com>
2021-09-19 23:59:49 -0500

    parse chrome selections

--- app/src/main/java/com/espial/espialshare/AddActivity.kt
+++ app/src/main/java/com/espial/espialshare/AddActivity.kt
@@ -3,10 +3,12 @@ package com.espial.espialshare
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.webkit.*
 import androidx.preference.PreferenceManager
+import java.net.URLEncoder
 
 class AddActivity : AppCompatActivity() {
     private lateinit var webView: WebView
@@ -54,19 +56,55 @@ class AddActivity : AppCompatActivity() {
     }
 
     private fun handleIntents() {
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-        val espialServerUrl = sharedPreferences.getString("espial_server_url", "")
-
         when (intent?.action) {
             Intent.ACTION_SEND -> {
                 if ("text/plain" == intent.type) {
-                    val intentUrl = intent.getStringExtra(Intent.EXTRA_TEXT)
-                    val intentSubject = intent.getStringExtra(Intent.EXTRA_SUBJECT) ?: ""
-                    val loadUrl = "$espialServerUrl/add?_hasData&url=$intentUrl&title=${intentSubject}"
-                    webView.loadUrl(loadUrl)
+                    val espialUrl = toEspialUrl(intent)
+                    webView.loadUrl(espialUrl)
                 }
             }
         }
     }
 
+    private fun toEspialUrl(intent: Intent): String {
+        val extraText = intent.getStringExtra(Intent.EXTRA_TEXT) ?: ""
+        val extraSubject = intent.getStringExtra(Intent.EXTRA_SUBJECT) ?: ""
+
+        val fromChromeSelection = tryParseChromeSelection(extraText)
+        val addParams = if(fromChromeSelection != null) {
+            AddParams(
+                URLEncoder.encode(fromChromeSelection.first, "utf-8"),
+                "",
+                URLEncoder.encode(fromChromeSelection.second, "utf-8")
+            )
+        } else {
+            AddParams(
+                URLEncoder.encode(extraText, "utf-8"),
+                URLEncoder.encode(extraSubject, "utf-8"),
+                ""
+            )
+        }
+        return toEspialUrl(addParams)
+    }
+
+    private fun toEspialUrl(addParams: AddParams): String {
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val espialServerUrl = sharedPreferences.getString("espial_server_url", "") ?: ""
+
+        return "$espialServerUrl/add?_hasData&url=${addParams.Url}&title=${addParams.Title}&description=${addParams.Description}"
+    }
+
+    private fun tryParseChromeSelection(srcText: String): Pair<String, String>? {
+        val res = srcText.split('\n')
+        if(res.count() > 1) {
+            val potentialUrl = res.last().trim().takeWhile { it != '#' }
+            if(Patterns.WEB_URL.matcher(potentialUrl).matches()) {
+                val description = res.dropLast(1).joinToString("\n").removeSurrounding("\"")
+                return Pair(potentialUrl, description)
+            }
+        }
+        return null
+    }
+
+    class AddParams (val Url: String, val Title: String, val Description: String)
 }
